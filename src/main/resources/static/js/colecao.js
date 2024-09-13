24462c777da018d1623d38031c4ea750feb2ee1d
eduardo <eduardodsxavier@gmail.com>
2024-09-12 21:38:17 -0300

    commit changes

--- src/main/resources/static/js/colecao.js
+++ src/main/resources/static/js/colecao.js
@@ -1,5 +1,5 @@
 // uma função que recebe ua lista de livros e o id da tag em html pra colocar a lista
-function colecaoLivros (livros, id) {
+export default function colecaoLivros (livros, id) {
     const colecao = document.getElementById(id)
     colecao.innerHTML += livros.map(livro => (
 	// mudar aqui as info dos livros que vão aparecer na tela
