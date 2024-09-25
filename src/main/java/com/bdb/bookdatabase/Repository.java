c6d454c984c212372c4e4881affc7f172f7f4c9a
marcelo914 <marcelosouza222244@gmail.com>
2024-09-24 21:01:25 -0300

    LoginController, UserService, UserRepository, Login.js and login.html hotfix. UserDTO Implementatio

--- src/main/java/com/bdb/bookdatabase/Repository.java
+++
@@ -1,8 +0,0 @@
-package com.bdb.bookdatabase;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.bdb.bookdatabase.model.User;
-
-public interface Repository extends JpaRepository<User, Long> {
-
-}
