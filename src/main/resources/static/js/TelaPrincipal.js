baaea68f52b3b0c107181759028c6b2549b06542
DanielCs1609 <131261876+DanielCs1609@users.noreply.github.com>
2024-11-19 17:55:38 -0300

    Mudanças JS

--- src/main/resources/static/js/TelaPrincipal.js
+++ src/main/resources/static/js/TelaPrincipal.js
@@ -31,3 +31,33 @@ function autoPlay() {
 }
 
 setInterval(autoPlay, 3000);
+
+//Js para o perfil 
+
+async function userName(id){
+    try{
+        const response = await fetch(`http://localhost:8080/user/userdata/${id}`);
+
+        if (!response.ok) {
+            throw new Error("Erro ao buscar o usuário");
+        }
+
+
+        const data = await response.json();
+
+        const user_name = data.name;
+
+
+        const elementName = document.getElementById('nome_img');
+        elementName.textContent = user_name;
+
+        console.log(`User Id: ${user_id}, User Name: ${user_name}`)
+    }catch(error){
+        console.error(error);
+    }
+}
+userName(1)
+
+document.getElementById("addBook").addEventListener("click", function() {
+    window.location.href = "AddBook";
+}); // fim do perfil
