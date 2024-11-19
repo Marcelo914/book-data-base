d83496b8944cfb1c6df364058e8b97e077bfb300
eduardo <eduardodsxavier@gmail.com>
2024-11-19 12:27:07 -0300

    book page show book in the database

--- src/main/resources/static/js/bookDetails.js
+++ src/main/resources/static/js/bookDetails.js
@@ -1,25 +1,32 @@
 // bookDetails.js
 document.addEventListener("DOMContentLoaded", function () {
     const q = window.location.href.split('?id=')[1];
-    console.log(q);
-    // Simulando dados do livro (substitua esses dados com os que vêm da API)
-    const bookData = {
-        title: "Harry Potter e a Pedra Filosofal",
-        author: "J.K. Rowling",
-        publisher: "Bloomsbury",
-        publishedDate: "1997-06-26",
-        description: "Harry Potter descobre que é um bruxo e embarca em uma aventura mágica na escola de Hogwarts.",
-        imageUrl: "https://m.media-amazon.com/images/I/81ibfYk4qmL._SY425_.jpg"
-    };
-
-    // Populando os elementos com dados do livro
-    document.getElementById("bookTitle").textContent = bookData.title;
-    document.getElementById("bookAuthor").textContent = "Autor: " + bookData.author;
-    document.getElementById("bookPublisher").textContent = "Editora: " + bookData.publisher;
-    document.getElementById("bookPublishedDate").textContent = "Data de Publicação: " + bookData.publishedDate;
-    document.getElementById("bookDescription").textContent = "Descrição: " + bookData.description;
-
-    // Carregando a imagem do livro
-    const bookImage = document.getElementById("bookImage");
-    bookImage.src = bookData.imageUrl || "https://m.media-amazon.com/images/I/81ibfYk4qmL._SY425_.jpg";
+    try {
+        fetch('http://localhost:8080/books/' + q)
+            .then(function (response) {
+                response.json()
+                    .then(function(x) { 
+                        if (x.title) {
+                            document.getElementById("bookTitle").textContent = x.title;
+                        }
+                        if (x.author) {
+                            document.getElementById("bookAuthor").textContent = "Autor: " + x.author;
+                        }
+                        if (x.publisher) {
+                            document.getElementById("bookPublisher").textContent = "Editora: " + x.publisher;
+                        }
+                        if (x.publishedDate) {
+                            document.getElementById("bookPublishedDate").textContent = "Data de Publicação: " + x.publishedDate;
+                        }
+                        if (x.description) {
+                            document.getElementById("bookDescription").textContent = "Descrição: " + x.description;
+                        }
+                        if (x.imageUrl) {
+                            document.getElementById("bookImage").src = x.imageUrl
+                        }
+                    }) 
+            })
+    } catch (e) {
+        console.log(e)
+    }
 });
